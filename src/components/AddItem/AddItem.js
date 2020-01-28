189d3d813992f051858c89612f51c4b5f0d2395d
Mr.Awwad <omarawwad010@gmail.com>
2020-01-28 03:25:29 +0200

    add-item image-compression & validation

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -3,6 +3,7 @@ import classes from './AddItem.module.css';
 import CategoriesDropDown from '../CategoriesDropDown/CategoriesDropDown';
 import ImageUpload from './ImageUpload/ImageUpload';
 import Button from '../../UI/Button/Button';
+import imageCompression from 'browser-image-compression';
 
 const AddItem = (props) => {
     const [imageUploadsState, setImageUploadsState] = useState
@@ -10,63 +11,96 @@ const AddItem = (props) => {
             {
                 mainImage: { url: null },
                 extra1: { url: null },
-                extra2: { url: null }
+                extra2: { url: null },
+                errors: null
             }
         );
     const [formInputsState, setFormInputsState] = useState
         ({
-            itemName: '',
-            itemPrice: '',
-            itemDesc: ''
+            itemName: { value: '' },
+            itemPrice: { value: '', errors: null },
+            itemDesc: { value: '' }
         });
+    const [overAllFormState, setOverAllFormState] = useState({ submitted: false });
 
     const [categoryState, setCategoryState] = useState('Categories');
     const [subCategoryState, setSubCategoryState] = useState('SubCategories');
 
     const formInputsHandler = (e, id) => {
-        setFormInputsState({ ...formInputsState, [id]: e.target.value })
-    }
-
-    const uploadImageHandler = (e, id) => {
-        const file = e.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setImageUploadsState({ ...imageUploadsState, [id]: { url: reader.result } })
+        let errors = null;
+        if (id === "itemPrice") {
+            const pattern = /^\d+$/;
+            const isValid = pattern.test(e.target.value);
+            if (!isValid) {
+                if (e.target.value === '') {
+                    errors = ['required field']
+                } else {
+                    errors = ['not a number']
+                }
             }
-            reader.readAsDataURL(file);
+            setFormInputsState({ ...formInputsState, [id]: { errors, value: e.target.value } });
+            return;
         }
-    }
-    const clearImageUploads = (id) => {
-        setImageUploadsState({ ...imageUploadsState, [id]: { url: null } })
+        setFormInputsState({ ...formInputsState, [id]: { ...formInputsState[id], value: e.target.value } })
     }
 
     const submitHandler = (e) => {
         e && e.preventDefault();
-        console.log();
+        setOverAllFormState({ submitted: true });
+        if (formInputsState.itemPrice.value.trim() === '') {
+            setFormInputsState({ ...formInputsState, itemPrice: { ...formInputsState['itemPrice'], errors: ['required field'] } })
+        }
+        if (imageUploadsState.mainImage.url === null) {
+            setImageUploadsState({ ...imageUploadsState, errors: ['main image is required'] })
+        }
+        if (formInputsState.itemPrice.errors === null && formInputsState.itemName.value.trim() !== '' && formInputsState.itemDesc.value.trim() !== '' && formInputsState.itemPrice.value.trim() !== '') {
+            if (imageUploadsState.mainImage.url !== null && imageUploadsState.errors === null && categoryState !== 'Categories') {
+                const options = {
+                    maxSizeMB: 1,
+                    maxWidthOrHeight: 1920,
+                    useWebWorker: true
+                }
+                Object.keys(imageUploadsState).forEach(async (key) => {
+                    if (key !== 'errors' && imageUploadsState[key].url) {
+                        const compressedFile = await imageCompression(await imageCompression.getFilefromDataUrl(imageUploadsState[key].url), options);
+                        console.log(compressedFile.size / 1000000);
+                        // upload data
+                    }
+                });
+
+
+            }
+        }
+
+
     }
     return (
         <section className={classes['add-item']}>
             <form className={classes['form']} onSubmit={(e) => submitHandler(e)} >
-                <input value={formInputsState.itemName} onChange={(e) => formInputsHandler(e, 'itemName')} placeholder="Item Name" className={classes['form__input']} />
-                <input value={formInputsState.itemPrice} onChange={(e) => formInputsHandler(e, 'itemPrice')} placeholder="Item Price" className={classes['form__input']} />
-                <textarea value={formInputsState.itemDesc} onChange={(e) => formInputsHandler(e, 'itemDesc')} placeholder="Description" className={classes['form__text-area']} />
-                <CategoriesDropDown 
-                setCategoryState= {setCategoryState} 
-                categoryState={categoryState}
-                setSubCategoryState={setSubCategoryState}
-                subCategoryState={subCategoryState}
+                <input value={formInputsState.itemName.value} onChange={(e) => formInputsHandler(e, 'itemName')} placeholder="Product Name" className={classes['form__input']} />
+                <span className={classes['form__error-message']}> {(formInputsState.itemName.value.trim() === '' && overAllFormState.submitted) && 'required field'} </span>
+                <input value={formInputsState.itemPrice.value} onChange={(e) => formInputsHandler(e, 'itemPrice')} placeholder="Product Price" className={classes['form__input']} />
+                <span className={classes['form__error-message']}> {formInputsState.itemPrice.errors && formInputsState.itemPrice.errors.join(' ')} </span>
+                <textarea value={formInputsState.itemDesc.value} onChange={(e) => formInputsHandler(e, 'itemDesc')} placeholder="Product Description" className={classes['form__text-area']} />
+                <span className={classes['form__error-message']}> {(formInputsState.itemDesc.value.trim() === '' && overAllFormState.submitted) && 'required field'} </span>
+                <CategoriesDropDown
+                    setCategoryState={setCategoryState}
+                    categoryState={categoryState}
+                    setSubCategoryState={setSubCategoryState}
+                    subCategoryState={subCategoryState}
                 />
+                <span className={classes['form__error-message']}> {overAllFormState.submitted && (categoryState === 'Categories') ? 'you must select a category' : null} </span>
                 <section className={classes['form__image-uploads']}>
-                    <ImageUpload url={imageUploadsState.mainImage.url} id="mainImage" clear={clearImageUploads} onChange={uploadImageHandler} />
-                    <ImageUpload url={imageUploadsState.extra1.url} id="extra1" clear={clearImageUploads} onChange={uploadImageHandler} />
-                    <ImageUpload url={imageUploadsState.extra2.url} id="extra2" clear={clearImageUploads} onChange={uploadImageHandler} />
+                    <ImageUpload url={imageUploadsState.mainImage.url} id="mainImage" imageState={imageUploadsState} setImageState={setImageUploadsState} />
+                    <ImageUpload url={imageUploadsState.extra1.url} id="extra1" imageState={imageUploadsState} setImageState={setImageUploadsState} />
+                    <ImageUpload url={imageUploadsState.extra2.url} id="extra2" imageState={imageUploadsState} setImageState={setImageUploadsState} />
                 </section>
+                <span className={classes['form__error-message']}> {imageUploadsState.errors && imageUploadsState.errors.join(', ')} </span>
                 <Button
                     onClick={submitHandler}
                     hoverable
-                    styles={{ color: '#fff', backgroundColor: '#4e002d', textAlign: 'center' }}
-                    hoverStyles={{ backgroundColor: '#ff0061', color: '#fff' }}
+                    styles={{ color: '#fff', backgroundColor: '#ff0061', textAlign: 'center' }}
+                    hoverStyles={{ backgroundColor: '#4e002d', color: '#fff' }}
                 >
                     Upload
                 </Button>
--- src/components/AddItem/ImageUpload/ImageUpload.js
+++ src/components/AddItem/ImageUpload/ImageUpload.js
@@ -3,13 +3,48 @@ import classes from './ImageUpload.module.css'
 import { ReactComponent as Plus } from '../../../assets/plus.svg'
 
 const imageUpload = (props) => {
+
+    const uploadImageHandler = (e, id) => {
+        const file = e.target.files[0];
+        if (file) {
+            if (validateImage(file)) {
+                props.setImageState({ ...props.imageState, errors: validateImage(file) })
+                return;
+            }
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                props.setImageState({ ...props.imageState, [id]: { url: reader.result }, errors: null })
+            }
+            reader.readAsDataURL(file);
+        }
+    }
+
+    const validateImage = (file) => {
+        let arr = [];
+        const fileExtension = file.name.substring(file.name.lastIndexOf('.') + 1);
+        const fileSize = file.size / 1000000;
+        const validExtensions = ['jpg', 'png', 'jpeg'];
+        if (fileSize > 3) {
+            arr.push('file is too big. (max 2mb per image)');
+        }
+        if (!validExtensions.includes(fileExtension)) {
+            arr.push(`only these extensions are allowed (${validExtensions.join(', ')})`)
+        }
+
+        return arr.length === 0 ? null : arr;
+    }
+
+    const clearImageUploads = (id) => {
+        props.setImageState({ ...props.imageState, [id]: { url: null } })
+    }
+
     return (
         <div className={classes['image-upload']}>
-            <input id={props.id} type="file" onClick={(e) => e.target.value = ''} onChange={(e) => props.onChange(e, props.id)} />
+            <input id={props.id} type="file" onClick={(e) => e.target.value = ''} onChange={(e) => uploadImageHandler(e, props.id)} />
             {props.url ? (
                 <div className={classes['image-upload__image-preview']} style={{ backgroundImage: `url(${props.url})` }}>
                     <div className={classes['image-preview__overlay']}>
-                        <a onClick={() => props.clear(props.id)} className={classes['overlay__links']} >Cancel</a>
+                        <a onClick={() => clearImageUploads(props.id)} className={classes['overlay__links']} >Cancel</a>
                         <label htmlFor={props.id} className={classes['overlay__links']} >Change</label>
                     </div>
                 </div>)
